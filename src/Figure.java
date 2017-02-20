bcc665521aa89b16c076d685af72fd0933b5f8e2
AnnaKohler <anna.kohler.610@gmail.com>
2017-02-20 17:54:48 +0300

    1

--- src/Figure.java
+++
@@ -1,7 +0,0 @@
-/**
- * Created by student1 on 16.01.17.
- */
-public interface Figure {
-    double getArea(double a, double b, double c);
-    double getPerimeter(double a, double b, double c);
-}
--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,26 @@
-/**
- * Created by student1 on 16.01.17.
- */
-public class Main {
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main{
     public static void main(String[] args) {
-        Figure triangle = new Figure() {
-            @Override
-            public double getArea(double a, double b, double c) {
-                return a*b*c;
-            }
-
-            @Override
-            public double getPerimeter(double a, double b, double c) {
-                return 2*a+2*b;
-            }
-        };
-        Figure rectangle = new Figure() {
-            @Override
-            public double getArea(double a, double b, double c) {
-                double p = getPerimeter(a, b, c)*2;
-                return Math.sqrt(p*(p-a)*(p-b)*(p-c));
-            }
-
-            @Override
-            public double getPerimeter(double a, double b, double c) {
-                return a+b+c;
-            }
-        };
-
-        Figure [] fArr = {rectangle, triangle};
-        for (Figure f:fArr
-             ) {
-            System.out.println(f.getArea(1, 2, 5));
+        HashMap<String, String> hM=new HashMap<>();
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        String key;
+        String value;
+        for(int i=0; i<n; i++){
+            key=sc.next();
+            value=sc.next();
+            hM.put(key, value);
         }
-
+        n=sc.nextInt();
+        for(int i=0; i<n; i++){
+            key=sc.next();
+            if(hM.get(key)!=null){
+                System.out.println(hM.get(key));
+                hM.remove(key);
+            }else System.out.println("null");
+        }
+        System.out.println(hM.size());
     }
 }
